171fca7902f4018d8a7bf4982d190b4d71ef7672
Pramath S.B <pramathsb@gmail.com>
2025-09-25 13:37:28 +0530

    update

--- mac.sh
+++ mac.sh
@@ -6,26 +6,27 @@ sudo /Applications/Install\ macOS\ Sequoia.app/Contents/Resources/createinstallm
 Chrome
 iTerm
 brew
-vscode
 firefox
+vscode
 postman
 gitkraken
-runnycat
-Dr.cleaner
 docker
 mongodb
-Istatmenu
 intelijce
 Android studio
 sourcetree
 wireguard
 httptoolkit
+mist
 
+runnycat
+Dr.cleaner
+Istatmenu
 
 brew install git
-brew install openjdk
 brew install nvm
-brew install ppm  
+brew install pnpm  
+brew install openjdk
 brew install jadx
 
 git config --global user.name "Pramath S.B"
